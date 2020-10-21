87b1bd072c7716b77870a7f6aa2bb3ab7ed087c9
Zjackeroo <jeffery_guo@hotmail.com>
2020-10-21 06:32:07 -0400

    static

--- js/script.js
+++ js/script.js
@@ -4,16 +4,17 @@ d3.csv('./data/coffee-house-chains.csv', d3.autoType).then(data=>{
     console.log('sorted data: ', sorted_data)
 
     const margin = {top: 20, right: 20, bottom: 20, left: 20};
-    const width = 700 - margin.left - margin.right,
+    const width = 650 - margin.left - margin.right,
           height = 500 - margin.top - margin.bottom;
 
     const xScale = d3.scaleBand()
-                     .domain(sorted_data.map(d=>d.company))
-                     .rangeRound([0, width]);
+                     .domain(data.map(d=>d.company))
+                     .range([0, width-5]);
 
-    // console.log(d3.extent(sorted_data, d=>d.stores))
+    // console.log(d3.extent(data, d=>d.stores))
+    // console.log(height)
     const yScale = d3.scaleLinear()
-                     .domain(d3.extent(sorted_data, d=>d.stores))
+                     .domain([0, d3.max(data, d=>(d.stores))])
                      .range([height, 0]);
     
     const svg = d3.select('.coffee_plot')
@@ -23,29 +24,30 @@ d3.csv('./data/coffee-house-chains.csv', d3.autoType).then(data=>{
                   .append('g')
                   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    // svg.selectAll('rect')
-    //     .data(sorted_data)
-    //     .enter()
-    //     .append('rect')
-    //     .attr('width', 50)
-    //     .attr('height', d=>d.stores)
-    //     .attr('x', (d,i)=>17+(i*82))
-    //     .attr('y', height-data.stores)
-    //     .attr('fill', 'orange')
-    //     .attr('cx', d=>xScale(d.company))
-    //     .attr('cy', d=>yScale(d.stores))
+    svg.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('width', 65)
+        .attr('height', d=>(height-yScale(d.stores)))
+        .attr('x', (d,i)=>25+(i*xScale.bandwidth()))
+        .attr('y', d=>(yScale(d.stores)))
+        // .attr('cx', d=>xScale(d.company))
+        // .attr('cy', d=>yScale(d.stores))
+        .attr('fill', '#6f4e37');
     
     const xAxis = d3.axisBottom()
                     .scale(xScale);
     const yAxis = d3.axisLeft()
                     .scale(yScale);
     svg.append('g')
+       .attr('transform', `translate(20, ${height})`)
        .attr('class', 'axis x-axis')
-       .call(xAxis)
-       .attr('transform', `translate(0, ${height})`);
+       .call(xAxis);
 
     svg.append('g')
        .attr('class', 'axis y-axis')
+       .attr("transform", "translate(20, 0)")
        .call(yAxis);
     
     yLabel = svg.append("text")
